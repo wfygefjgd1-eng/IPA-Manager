babf1e985015cc478eea28fa0d6c528228bb8b1f
wfygefjgd1-eng <wfygefjgd1-eng@users.noreply.github.com>
2026-08-19 15:09:54 +0800

    Use sec_identity_create_with_certificates (iOS SDK API)

--- Sources/Installer/TLSIdentityHelper.m
+++ Sources/Installer/TLSIdentityHelper.m
@@ -1,11 +1,20 @@
 #import "TLSIdentityHelper.h"
 #import <Security/SecIdentity.h>
+#import <Security/SecProtocolTypes.h>
 
 void IPMSetTLSIdentity(sec_protocol_options_t options, SecIdentityRef identity) {
     if (options == NULL || identity == NULL) {
         return;
     }
-    sec_identity_t secIdentity = sec_identity_create_with_identity(identity);
+
+    // Extract the leaf certificate from the identity
+    SecCertificateRef cert = NULL;
+    if (SecIdentityCopyCertificate(identity, &cert) != errSecSuccess || cert == NULL) {
+        return;
+    }
+
+    sec_identity_t secIdentity = sec_identity_create_with_certificates(cert, NULL);
+    CFRelease(cert);
     if (secIdentity != NULL) {
         sec_protocol_options_set_local_identity(options, secIdentity);
     }
